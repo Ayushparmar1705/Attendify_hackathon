5654fad3625b0d02dc77879fc03cd6f00b79a700
Ayush_Parmar <154075430+Ayushparmar1705@users.noreply.github.com>
2025-09-08 23:19:58 +0530

    add crud operation for class

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,14 @@
+// import the express library
 const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
+// configuration file for database
 const conn = require("./dbConfig/dbConfig");
-
+// create the post api
 app.post("/admin/signup", (req, res) => {
+    // get the data from the body;
     const data = req.body;
     const sql = "INSERT INTO admin_signup(name,email,password) VALUES(?,?,?)";
     conn.query(sql, [data.name, data.email, data.password], (err, result) => {
@@ -13,12 +16,12 @@ app.post("/admin/signup", (req, res) => {
             return res.status(500).send({ message: err });
         }
         else {
-
             return res.status(200).send({ message: "Admin created succesfully" });
         }
     });
 
 })
+// create the api for get
 app.get("/admin/login", (req, res) => {
     const data = req.body;
     const sql = "SELECT * FROM admin_signup WHERE email = ? AND password = ?";
@@ -27,15 +30,97 @@ app.get("/admin/login", (req, res) => {
             return res.status(500).send({ message: err });
         }
         else {
-            if(result.length>0)
-            {
-                
+            if (result.length > 0) {
+
             }
             return res.status(200).send({ message: result.id });
         }
     });
-
+    // 
+})
+// create the api for edit
+app.put("/admin/addclass", (req, res) => {
+    const data = req.body;
+    const sql = "INSERT INTO Add_Class(admin_id , class_name) VALUES(?,?)";
+    sql.query(sql, [data.admin_id, data.classname], (err, result) => {
+        if (err) {
+            return res.status(500).send({ message: err });
+        } else {
+            return res.status(200).send({ message: result });
+        }
+    })
 })
 
 
+app.get("/admin/manageclass", (req, res) => {
+    const data = req.body;
+    const sql = "SELECT * FROM Add_Class";
+    conn.query(sql, (err, result) => {
+        if (err) {
+            return res.status(500).send({ message: err });
+        }
+        else {
+            if (result.length > 0) {
+
+            }
+            return res.status(200).send({ message: result.id });
+        }
+    });
+})
+// create the api for the delete
+app.delete("/admin/deleteclass/:id", (req, res) => {
+    // get the id from the url
+    const data = res.params;
+    const sql = "DELETE FROM Add_class WHERE id = ?";
+    sql.query(sql, [id], (err, result) => {
+        if (err) {
+            return res.status(500).send({ message: err });
+        } else {
+            return res.status(200).send({ message: "class deleted succesfully" });
+        }
+    })
+})
+app.delete("/admin/editclass/:id", (req, res) => {
+    const data = req.body;
+    const sql = "UPDATE Add_class WHERE id = ?";
+    sql.query(sql, [id], (err, result) => {
+        if (err) {
+            return res.status(500).send({ message: err });
+        } else {
+            return res.status(200).send({ message: "class update succesfully" });
+        }
+    })
+})
+
+
+
+
+app.get("/admin/manageclass", (req, res) => {
+    const data = req.body;
+    const sql = "SELECT * FROM Add_Class";
+    conn.query(sql, (err, result) => {
+        if (err) {
+            return res.status(500).send({ message: err });
+        }
+        else {
+            if (result.length > 0) {
+
+            }
+            return res.status(200).send({ message: result.id });
+        }
+    });
+})
+
+app.put("/admin/addteacher", (req, res) => {
+    const data = req.body;
+    const sql = "INSERT INTO Add_Class(admin_id , class_name) VALUES(?,?)";
+    sql.query(sql, [data.admin_id, data.classname], (err, result) => {
+        if (err) {
+            return res.status(500).send({ message: err });
+        } else {
+            return res.status(200).send({ message: result });
+        }
+    })
+})
+// listen the server to 5000  
 app.listen(5000);
